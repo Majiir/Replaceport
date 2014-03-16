9a9a3f973800c82d94527db1da537147334625b4
Majiir Paktu <majiir@nabaal.net>
2014-03-15 21:21:05 -0400

    Bootswatch Cosmo theme from LESS sources

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,12 @@ module.exports = function (grunt) {
 	var jshint_files = [
 		'**/*.js',
 		'!**/node_modules/**',
+		'!**/bower_components/**',
 	];
+
+	var less_files = {
+		'static/main.css': 'less/main.less',
+	};
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
@@ -28,10 +33,42 @@ module.exports = function (grunt) {
 				'node': true,
 			},
 		},
+		less: {
+			options: {
+				paths: [
+					'less',
+					'bower_components',
+				],
+			},
+			dev: {
+				files: less_files,
+				options: {
+					sourceMap: true,
+					sourceMapFilename: 'static/main.css.map',
+					sourceMapURL: 'main.css.map',
+				},
+			},
+			dist: {
+				files: less_files,
+				options: {
+					compress: true,
+					cleancss: true,
+					report: 'min',
+				},
+			},
+		},
 		watch: {
 			jshint: {
 				files: jshint_files,
 				tasks: ['jshint'],
+				options: {
+					atBegin: true,
+					interrupt: true,
+				},
+			},
+			less: {
+				files: ['less/**/*.less'],
+				tasks: ['less:dev'],
 				options: {
 					atBegin: true,
 					interrupt: true,
@@ -49,9 +86,13 @@ module.exports = function (grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('default', [
+		'jshint',
+		'less:dist',
+	]);
 	grunt.registerTask('dev', ['watch']);
 
 };
